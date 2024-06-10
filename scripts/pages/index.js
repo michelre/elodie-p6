048fc9a74d55630d0c545ba5c36c25421ee45deb
ElodieAllegro <124352648+ElodieAllegro@users.noreply.github.com>
2024-06-10 11:04:52 +0200

    10/06

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,29 +1,7 @@
     async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+        const response = await fetch('../data/photographers.json')
+        const data = await response.json()
+        return ({photographers: data.photographers})
     }
 
     async function displayData(photographers) {
@@ -32,7 +10,47 @@
         photographers.forEach((photographer) => {
             const photographerModel = photographerTemplate(photographer);
             const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
+
+    
+            //element link
+            const linkElement = document.createElement("a");
+            linkElement.setAttribute("href", `photographer.html?id=${photographer.id}`);
+            linkElement.appendChild(userCardDOM);
+
+
+            //element location 
+            const locationInfo = document.createElement("div");
+            locationInfo.classList.add("location_info");
+
+            const locationElement = document.createElement("h4");
+            locationElement.textContent = `${photographer.city}, ${photographer.country}`;
+
+            locationInfo.appendChild(locationElement);
+            userCardDOM.appendChild(locationInfo);
+
+            //element tagline
+            const taglineInfo = document.createElement("div");
+            taglineInfo.classList.add("tagline_info");
+
+            const taglineElement = document.createElement("p");
+            taglineElement.textContent = photographer.tagline;
+
+            taglineInfo.appendChild(taglineElement);
+            userCardDOM.appendChild(taglineInfo);
+
+            //element price
+            const priceInfo = document.createElement("div");
+            priceInfo.classList.add("price_info");
+
+            const priceElement = document.createElement("p");
+            priceElement.textContent = photographer.price + "€/jour";
+
+            priceInfo.appendChild(priceElement);
+            userCardDOM.appendChild(priceInfo);
+
+           
+            
+            photographersSection.appendChild(linkElement);
         });
     }
 
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,83 @@
-//Mettre le code JavaScript lié à la page photographer.html
+
+ //Mettre le code JavaScript lié à la page photographer.htmlconst parsedUrl = new URL(window.location.href);
+
+const parsedUrl = new URL(window.location.href);
+const id = parseInt(parsedUrl.searchParams.get("id"));
+
+console.log(id);
+
+
+// Fonction pour récupérer les médias associés au photographe
+const fetchMedia = async () => {
+    const response = await fetch('../data/photographers.json')
+    const data = await response.json()
+
+    const media = data.media.filter(media => media.photographerId === id)
+    console.log(media)
+}
+
+fetchMedia()
+
+
+// Fonction pour récupérer les données d'un photographe à partir de son identifiant
+const fetchPhotograph = async () => {
+    const response = await fetch('../data/photographers.json');
+    const data = await response.json();
+    const photographer = data.photographers.find(photographer => photographer.id === id);
+   
+    console.log(photographer);
+    return photographer;
+}
+
+
+// Fonction pour afficher les données d'un photographe
+const displayProfil = (photographer) => {
+    const photographer_section = document.querySelector(".photograph-header");
+    
+    const picture = `assets/photographers/${photographer.portrait}`;
+    const divPhotographer = document.createElement("div");
+    divPhotographer.classList.add("photographer-content");
+    
+    // Ajouter le nom du photographe dans un H1
+    const h1 = document.createElement("h1");
+    h1.textContent = photographer.name;
+    photographer_section.appendChild(h1);
+
+    // Ajouter la localisation (ville, pays) dans un H4
+    const locationElement = document.createElement("h4");
+    locationElement.textContent = `${photographer.city}, ${photographer.country}`;
+    photographer_section.appendChild(locationElement);
+
+    // Ajouter le slogan du photographe dans un paragraphe
+    const taglineElement = document.createElement("p");
+    taglineElement.textContent = photographer.tagline;
+    photographer_section.appendChild(taglineElement);
+   
+    //creer une div pour mettre l'image a l'interieur
+    const divPicturePhotographer = document.createElement("div");
+    divPicturePhotographer.classList.add("photographer-picture");
+    // Ajouter l'image du photographe
+    const img = document.createElement( 'img' );
+    img.setAttribute("src", picture)
+    divPicturePhotographer.appendChild(img);
+    photographer_section.appendChild(divPicturePhotographer);
+    photographer_section.appendChild(divPhotographer);
+
+    divPhotographer.appendChild(h1);
+    divPhotographer.appendChild(locationElement);
+    divPhotographer.appendChild(taglineElement);
+}
+
+
+// Fonction pour afficher les médias du photographe
+
+
+// Initialiser les données et afficher le profil 
+const init = async () => {
+    const photographer = await fetchPhotograph();
+    displayProfil(photographer);
+
+}
+
+init();
+
