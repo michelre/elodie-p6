25df476bd1a8d693da4f5e226dd76d3fca94277d
ElodieAllegro <124352648+ElodieAllegro@users.noreply.github.com>
2024-07-04 14:58:28 +0200

    4/07

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,12 @@
+
+const modal = document.getElementById("contact_modal");
+
 function displayModal() {
-    const modal = document.getElementById("contact_modal");
+    
 	modal.style.display = "block";
 }
 
 function closeModal() {
-    const modal = document.getElementById("contact_modal");
+    
     modal.style.display = "none";
 }
